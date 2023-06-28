2d54ea1fdd707739c86eb659109b23cd5d03e926
Adrian Besciak <adrian.besciak99@gmail.com>
2023-06-28 20:30:32 +0200

    uncommented part of measurement script

--- measurements.sh
+++ measurements.sh
@@ -8,18 +8,18 @@ CHANNELS=2
 HEADER='hosts+routers+channels+cost'
 
 MEASUREMENTS_DIR='measurements'
-# CHANGING_ROUTERS_NO_FILE=$MEASUREMENTS_DIR/changing_routers_no2.csv
+ CHANGING_ROUTERS_NO_FILE=$MEASUREMENTS_DIR/changing_routers_no.csv
 
-# echo $HEADER > $CHANGING_ROUTERS_NO_FILE
-# for ROUTERS in $(seq 2 20)
-# do
-#     ./clearNetbox.sh
-#     echo \n\n\n\n\nGenerating topology:
-#     echo \ndragonfly_topology.py --setup --hosts=$HOSTS --routers=$ROUTERS --channels=$CHANNELS
-#     python3 dragonfly_topology.py --setup --hosts=$HOSTS --routers=$ROUTERS --channels=$CHANNELS
-#     COST=`python3 dragonfly_topology.py --costs`
-#     echo ${HOSTS}+${ROUTERS}+${CHANNELS}+${COST} >> $CHANGING_ROUTERS_NO_FILE
-# done
+ echo $HEADER > $CHANGING_ROUTERS_NO_FILE
+ for ROUTERS in $(seq 2 20)
+ do
+     ./clearNetbox.sh
+     echo \n\n\n\n\nGenerating topology:
+     echo \ndragonfly_topology.py --setup --hosts=$HOSTS --routers=$ROUTERS --channels=$CHANNELS
+     python3 dragonfly_topology.py --setup --hosts=$HOSTS --routers=$ROUTERS --channels=$CHANNELS
+     COST=`python3 dragonfly_topology.py --costs`
+     echo ${HOSTS}+${ROUTERS}+${CHANNELS}+${COST} >> $CHANGING_ROUTERS_NO_FILE
+ done
 
 
 CHANGING_CHANNELS_NO_FILE=$MEASUREMENTS_DIR/changing_channels_no.csv
